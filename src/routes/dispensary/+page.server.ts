c0e23387285e1f0eee6d5b0d2b436023ce1da8f0
Winston Wan <winst.wan@gmail.com>
2023-12-15 01:27:17 -0500

    Install steeze-ui/icons and tailwindcss/forms, frame up dispensary list page

--- src/routes/dispensary/+page.server.ts
+++
@@ -1,7 +0,0 @@
-export const actions = {
-    create: async ({ request }) => {
-        const data = await request.formData()
-        const formName = data.get('name')
-        return { success: true, name: formName }
-    },
-}
--- src/routes/dispensary/+page.svelte
+++ src/routes/dispensary/+page.svelte
@@ -1,19 +1,29 @@
-<script>
-    export let form
+<script lang="ts">
+    import { Icon } from '@steeze-ui/svelte-icon'
+    import { MagnifyingGlass } from '@steeze-ui/heroicons'
+
+    export let data
 </script>
 
-<h1>Create Dispensary</h1>
-<form method="POST" action="?/create">
-    <label>
-        Name
-        <input name="name" type="input" />
-    </label>
-    <button>Save</button>
-</form>
-<h1>
-    Results
-    {#if form?.success}
-        ✅
-    {/if}
-</h1>
-<pre>{JSON.stringify(form, null, 2)}</pre>
+<h1>Browse Dispensaries</h1>
+<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+    <div class="input-group-shim">
+        <Icon src={MagnifyingGlass} size="24px" theme="outline" />
+    </div>
+    <input type="search" placeholder="Search dispensaries..." />
+    <button class="variant-filled-secondary">Search</button>
+</div>
+{#each data.dispensariesPromise.data || [] as dispensary}
+    <div class="card">
+        <header class="card-header">
+            {dispensary.name}
+        </header>
+        <section class="p-4">
+            <p>{dispensary.address_1}</p>
+            <p>{dispensary.address_2}</p>
+            <p>{dispensary.city}</p>
+            <p>{dispensary.postal_code}</p>
+        </section>
+        <footer class="card-footer">(footer)</footer>
+    </div>
+{/each}
