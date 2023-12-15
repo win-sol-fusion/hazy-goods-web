13d8c4c815af4611ac51c8002e0d7a0a4e2b7034
Solomon Montagno <sjmontagno@gmail.com>
2023-12-15 12:48:14 -0500

    Added form to page.svelte

--- src/routes/dispensary/add/+page.svelte
+++ src/routes/dispensary/add/+page.svelte
@@ -2,18 +2,44 @@
     export let form
 </script>
 
-<h1>Create Dispensary</h1>
+<h1>Add Dispensary</h1>
 <form method="POST" action="?/create">
-    <label>
-        Name
-        <input name="name" type="input" />
-    </label>
-    <button>Save</button>
+  <label>
+    Dispensary Name
+    <input name="name" type="input" />
+  </label>
+  <label>
+    Address
+    <input name="address" type="input" />
+  </label>
+  <label>
+    City 
+    <input name="city" type="input"/>
+  </label>
+  <label>
+    State
+    <input name="state" type="input"/>
+  </label>
+  <label>
+    Zip Code
+    <input name='zip' type="input" />
+  </label>
+  <label>
+    Status
+    <input name="status" type="input"/>
+  </label>
+  <label>
+    Created By
+    <input name="createdBy" type="input">
+  </label>
+  <button>Save</button>
 </form>
+
 <h1>
     Results
     {#if form?.success}
         ✅
     {/if}
 </h1>
+
 <pre>{JSON.stringify(form, null, 2)}</pre>
