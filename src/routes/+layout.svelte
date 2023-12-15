50487567c641048ba5d165dc1d964e7358494339
Winston Wan <84503075+winstwan@users.noreply.github.com>
2023-12-15 00:34:23 -0500

    Followed Skeleton's AppShell setup instructions (#7)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
-<script>
+<script lang="ts">
     import '../app.pcss'
+    import { AppBar, AppShell } from '@skeletonlabs/skeleton'
+    import type { AfterNavigate } from '@sveltejs/kit'
+    import { afterNavigate } from '$app/navigation'
+
+    // Scroll to Top on Navigation
+    afterNavigate((params: AfterNavigate) => {
+        const isNewPage = params.from?.url.pathname !== params.to?.url.pathname
+        const elemPage = document.querySelector('#page')
+        if (isNewPage && elemPage !== null) {
+            elemPage.scrollTop = 0
+        }
+    })
 </script>
 
-<slot />
+<AppShell>
+    <svelte:fragment slot="header">
+        <AppBar>Hazy Goods</AppBar>
+    </svelte:fragment>
+    <svelte:fragment slot="sidebarLeft">
+        <!-- Hidden below Tailwind's large breakpoint -->
+        <div id="sidebar-left" class="hidden lg:block">Sidebar</div>
+    </svelte:fragment>
+    <main>
+        <slot />
+    </main>
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,5 @@
 <script>
     export let data
-    import { AppBar, AppShell } from '@skeletonlabs/skeleton'
 </script>
-
-<AppBar
-    gridColumns="grid-cols-3"
-    slotDefault="place-self-center"
-    slotTrail="place-content-end"
->
-    <svelte:fragment slot="lead">(icon)</svelte:fragment>
-    (title)
-    <svelte:fragment slot="trail">(actions)</svelte:fragment>
-</AppBar>
 
 <pre>{JSON.stringify(data.thing1, null, 2)}</pre>
