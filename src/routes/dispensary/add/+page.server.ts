03482652ce826ec85550e52446151aed65335472
Solomon Montagno <sjmontagno@gmail.com>
2023-12-15 12:47:54 -0500

    Added dispensary form functionality

--- src/routes/dispensary/add/+page.server.ts
+++ src/routes/dispensary/add/+page.server.ts
@@ -1,7 +1,39 @@
+import supabase from "$lib/supabase/client";
+
 export const actions = {
   create: async ({ request }) => {
-    const data = await request.formData()
-    const formName = data.get("name")
-    return { success: true, name: formName }
-  }
-}
+    try {
+      const data = await request.formData();
+      const formName = data.get("name");
+      const formAddress = data.get("address");
+      const formCity = data.get("city");
+      const formState = data.get("state");
+      const formZip = data.get("zip");
+      const formStatus = data.get("status");
+      const formCreatedBy = data.get("createdBy");
+
+      const { data: insertData, error } = await supabase
+        .from("dispensary")
+        .insert([
+          {
+            name: formName,
+            address_1: formAddress,
+            city: formCity,
+            region_id: formState,
+            postal_code: formZip,
+            validation_status_id: formStatus,
+            created_by: formCreatedBy
+          },
+        ]);
+
+      if (error) {
+        console.error("Error inserting data into supabase:", error.message);
+        return { success: false, error: "Failed to insert data into Supabase" };
+      }
+      return { success: true, data: insertData };
+    } catch (error) {
+      console.error("Error processing form submission:", error.message);
+      return { success: false, error: "Failed to process form submission" };
+    }
+  },
+};
