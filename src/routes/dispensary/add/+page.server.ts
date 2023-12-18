d9afae520017972f7ed7981a3dbe2e9c2a40fd7a
Solomon Montagno <sjmontagno@gmail.com>
2023-12-18 17:44:00 -0500

    add state drop down menu

--- src/routes/dispensary/add/+page.server.ts
+++ src/routes/dispensary/add/+page.server.ts
@@ -1,34 +1,38 @@
-import supabase from "$lib/supabase/client";
+import supabase from '$lib/supabase/client'
 
 export const actions = {
-  create: async ({ request }) => {
-    try {
-      const data = await request.formData();
-      const formName = data.get("name");
-      const formAddress = data.get("address");
-      const formCity = data.get("city");
-      const formState = data.get("state");
-      const formZip = data.get("zip");
-      const formStatus = data.get("status");
-      const formCreatedBy = data.get("createdBy");
+    create: async ({ request }) => {
+        try {
+            const data = await request.formData()
+            const formName = data.get('name')
+            const formAddress = data.get('address')
+            const formCity = data.get('city')
+            const formState = data.get('state')
+            const formZip = data.get('zip')
 
-      const { data: insertData, error } = await supabase
-        .from("dispensary")
-        .insert([
-          {
-            name: formName,
-            address_1: formAddress,
-            city: formCity,
-            region_id: formState,
-            postal_code: formZip,
-            validation_status_id: 2, //pending
-            created_by: "22222222-2222-2222-2222-222222222222", // placeholder user id
-          },
-        ]);
-      return { success: true, data: insertData };
-    } catch (error) {
-      console.error("Error processing form submission:", error.message);
-      return { success: false, error: "Failed to process form submission" };
-    }
-  },
-};
+            const { data: insertData, error } = await supabase
+                .from('dispensary')
+                .insert([
+                    {
+                        name: formName,
+                        address_1: formAddress,
+                        city: formCity,
+                        region_id: formState,
+                        postal_code: formZip,
+                        validation_status_id: 2, //pending
+                        created_by: '22222222-2222-2222-2222-222222222222', // placeholder user id
+                    },
+                ])
+                if(error) {
+                  console.error("Error inserting date into supabase:", error.message)
+                }
+            return { success: true, data: insertData }
+        } catch (error: any) {
+            console.error('Error processing form submission:', error.message)
+            return {
+                success: false,
+                error: 'Failed to process form submission',
+            }
+        }
+    },
+}
--- src/routes/dispensary/add/+page.svelte
+++ src/routes/dispensary/add/+page.svelte
@@ -1,38 +1,75 @@
-<script>
+<script lang="ts">
     export let form
+    export let data
+    let selectedStateId: any = null
+
+    function handleSelectState(event: any) {
+        event.preventDefault()
+        const value = event.target.value
+        selectedStateId = value === '' ? null : parseInt(value, 10)
+        console.log('Selected State ID1:', selectedStateId)
+    }
+
+    function handleSubmit(event: Event) {
+        event.preventDefault()
+        console.log('Form Submitted')
+
+        if (selectedStateId !== null) {
+            console.log('Selected State ID2:', selectedStateId)
+            const formData = new FormData(
+                event.currentTarget as HTMLFormElement
+            )
+            formData.set('region_id', selectedStateId.toString()) 
+            fetch('?/create', {
+                method: 'POST',
+                body: formData,
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log('Data inserted successfully:', data)
+                })
+                .catch((error) => {
+                    console.error('Error inserting data:', error)
+                })
+        } else {
+            console.error('No state selected')
+        }
+    }
 </script>
 
 <h1>Add Dispensary</h1>
-<form method="POST" action="?/create">
-  <label>
-    Dispensary Name
-    <input name="name" type="input" />
-  </label>
-  <label>
-    Address
-    <input name="address" type="input" />
-  </label>
-  <label>
-    City 
-    <input name="city" type="input"/>
-  </label>
-  <label>
-    State
-    <input name="state" type="input"/>
-  </label>
-  <label>
-    Zip Code
-    <input name='zip' type="input" />
-  </label>
-  <label>
-    Status
-    <input name="status" type="input"/>
-  </label>
-  <label>
-    Created By
-    <input name="createdBy" type="input">
-  </label>
-  <button>Save</button>
+<form on:submit={handleSubmit}>
+    <label>
+        Dispensary Name
+        <input name="name" type="input" />
+    </label>
+    <label>
+        Address
+        <input name="address" type="input" />
+    </label>
+    <label>
+        City
+        <input name="city" type="input" />
+    </label>
+    <label for="selectedStates">
+        State
+        <select
+            id="selectedStates"
+            bind:value={selectedStateId}
+            on:change={handleSelectState}
+        >
+            <option value="">Select a state</option>
+            {#each data.states as state (state.id)}
+                <option value={state.id}>{state.name}</option>
+            {/each}
+        </select>
+    </label>
+    <label>
+        Zip Code
+        <input name="zip" type="input" />
+    </label>
+
+    <button type="submit">Save</button>
 </form>
 
 <h1>
