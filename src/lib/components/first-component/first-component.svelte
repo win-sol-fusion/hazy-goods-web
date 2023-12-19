a9077884d9fbea40dddaba98671b557cf0a82dae
Winston Wan <84503075+winstwan@users.noreply.github.com>
2023-12-18 20:59:10 -0500

    our first custom component (#10)

---
+++ src/lib/components/first-component/first-component.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let firstName: string
+    export let inputName: string
+    export let errorMessage: string | undefined
+</script>
+
+<div class="bg-primary-500 p-2">
+    <p class="bg-secondary-500">{inputName} for {firstName}</p>
+    {#if errorMessage}
+        <p class="text-error-800">{errorMessage}</p>
+    {/if}
+</div>
