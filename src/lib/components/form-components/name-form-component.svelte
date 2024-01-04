24c9e4bede6e38d77a6161750c018b8ec3e92da8
Solomon Montagno <sjmontagno@gmail.com>
2024-01-04 15:12:55 -0500

    Create reusable name component with validation

--- src/lib/components/form-components/name-form-component.svelte
+++ src/lib/components/form-components/name-form-component.svelte
@@ -7,7 +7,8 @@
     const errorHandler = (e: any) => {
         const alphanumericRegex = /^[a-zA-Z0-9]+$/
         const inputValue = e.target.value
-
+        let maxLength = 50
+        let minLength = 1
         if (inputValue.trim() === "") {
             nameError.push('Name is required')
         }
@@ -15,12 +16,23 @@
         if (!alphanumericRegex.test(inputValue)) {
             nameError.push('Invalid input. Please use numbers or letters')
         }
+
+        if(inputValue.length  > maxLength ) {
+            nameError.push('There is a 50 character limit')
+        }
+        
+        if(inputValue.length <= minLength) {
+            nameError.push('Must be at least 2 characters')
+        }
+        
     }
 </script>
 
 <div>
-    {#if nameError}
-        <p>{nameError}</p>
+    {#if nameError.length > 0}
+    {#each nameError as error (error)}
+        <p>{error}</p>
+        {/each}
     {/if}
     <label
         >{labelName}
