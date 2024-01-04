a8df505f979a0efcf5a331773bc5ebcbc8e9fd65
Solomon Montagno <sjmontagno@gmail.com>
2024-01-04 18:02:06 -0500

    Working validations but error with name prepopulating

--- src/lib/components/form-components/name-form-component.svelte
+++ src/lib/components/form-components/name-form-component.svelte
@@ -1,44 +1,56 @@
 <script lang="ts">
+    import { createEventDispatcher } from 'svelte'
+
     export let labelName: string | undefined
     export let inputName: string | undefined
-    export let nameError: string[] = []
     export let placeholderName: string | undefined
 
+    export let nameErrors: string[] = []
+    const dispatch = createEventDispatcher()
+
     const errorHandler = (e: any) => {
-        const alphanumericRegex = /^[a-zA-Z0-9]+$/
+        nameErrors = []
+
+        const alphanumericRegex = /^[a-zA-Z0-9\s]+$/
+
         const inputValue = e.target.value
         let maxLength = 50
-        let minLength = 1
-        if (inputValue.trim() === "") {
-            nameError.push('Name is required')
+        let minLength = 2
+        if (inputValue.trim() === '') {
+            nameErrors.push('Name is required')
         }
 
         if (!alphanumericRegex.test(inputValue)) {
-            nameError.push('Invalid input. Please use numbers or letters')
+            nameErrors.push('Invalid input. Please use numbers or letters')
         }
 
-        if(inputValue.length  > maxLength ) {
-            nameError.push('There is a 50 character limit')
+        if (inputValue.length > maxLength) {
+            nameErrors.push('There is a 50 character limit')
         }
-        
-        if(inputValue.length <= minLength) {
-            nameError.push('Must be at least 2 characters')
+
+        if (inputValue.length <= minLength) {
+            nameErrors.push('Must be at least 2 characters')
         }
-        
+        dispatch('inputError', { inputName, nameErrors })
+    }
+
+    let inputValue = inputName || ""
+    $: {
+        inputValue
     }
 </script>
 
 <div>
-    {#if nameError.length > 0}
-    {#each nameError as error (error)}
-        <p>{error}</p>
+    {#if nameErrors.length > 0}
+        {#each nameErrors as error (error)}
+            <p>{error}</p>
         {/each}
     {/if}
     <label
         >{labelName}
         <input
             name={inputName}
-            value={inputName}
+            bind:value={inputValue}
             type="input"
             placeholder={placeholderName}
             on:input={errorHandler}
