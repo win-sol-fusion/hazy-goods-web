4f6484466ace44adb5f0b08354b91e9ff7ff4d74
Winston Wan <84503075+winstwan@users.noreply.github.com>
2024-01-04 19:55:22 -0500

    Svelte storybook (#13)
    * ran 'npx storybook init'
    * Storybook story for dispensary card, styles not working yet

--- src/routes/dispensary/+page.svelte
+++ src/routes/dispensary/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { Icon } from '@steeze-ui/svelte-icon'
     import { MagnifyingGlass } from '@steeze-ui/heroicons'
-    import FirstComponent from '$lib/components/first-component/first-component.svelte'
+    import DispensaryCard from '$lib/components/dispensary-card/dispensary-card.svelte'
 
     export let data
 </script>
@@ -15,20 +15,5 @@
     <button class="variant-filled-secondary">Search</button>
 </div>
 {#each data.dispensariesQuery.data || [] as dispensary}
-    <div class="card">
-        <header class="card-header">
-            <FirstComponent
-                inputName="Title"
-                firstName={dispensary.name}
-                errorMessage={dispensary.city}
-            />
-        </header>
-        <section class="p-4">
-            <p>{dispensary.address_1}</p>
-            <p>{dispensary.address_2}</p>
-            <p>{dispensary.city}</p>
-            <p>{dispensary.postal_code}</p>
-        </section>
-        <footer class="card-footer">(footer)</footer>
-    </div>
+    <DispensaryCard {dispensary} />
 {/each}
