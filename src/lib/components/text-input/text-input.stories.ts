2a673e21b8a8b710b861e83f651baf9ee33ce8c0
Winston Wan <winst.wan@gmail.com>
2024-01-06 17:15:59 -0500

    Style errors for text input

--- src/lib/components/text-input/text-input.stories.ts
+++ src/lib/components/text-input/text-input.stories.ts
@@ -1,4 +1,7 @@
+import StoryDecorator from './story-decorator.svelte'
 import TextInput from './text-input.svelte'
+
+let value = 'foo'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
@@ -6,13 +9,24 @@ export default {
     component: TextInput,
     tags: ['autodocs'],
     argTypes: {},
+    // decorators: [() => StoryDecorator],
 }
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default = {
     args: {
+        inputValue: value,
         label: 'First Name',
-        inputValue: '',
         placeholder: 'type your first name here',
+        validationErrors: [],
+    },
+}
+
+export const Errors = {
+    args: {
+        inputValue: value,
+        label: 'First Name',
+        placeholder: 'type your first name here',
+        validationErrors: ['This is a validation error'],
     },
 }
--- src/lib/components/text-input/text-input.svelte
+++ src/lib/components/text-input/text-input.svelte
@@ -1,29 +1,33 @@
 <script lang="ts">
-    import { createEventDispatcher } from 'svelte'
+    // import { createEventDispatcher } from 'svelte'
 
     export let label: string | undefined
     export let inputName: string | undefined
     export let inputValue: string | undefined
     export let placeholder: string | undefined
-
     export let validationErrors: string[] = []
-    const dispatch = createEventDispatcher()
+    // const dispatch = createEventDispatcher()
+
+    let inputErrorStyles = ''
+    $: inputErrorStyles = validationErrors.length > 0 ? 'border-error-500' : ''
 </script>
 
 <div>
-    {#if validationErrors.length > 0}
-        {#each validationErrors as error (error)}
-            <p>{error}</p>
-        {/each}
-    {/if}
     <label class="label"
         >{label}
         <input
-            class="input"
+            class="input border-2 {inputErrorStyles}"
             name={inputName}
             bind:value={inputValue}
             type="input"
             {placeholder}
         />
     </label>
+    <div class="">
+        {#if validationErrors.length > 0}
+            {#each validationErrors as error (error)}
+                <p class="font-light text-sm text-error-500 italic">{error}</p>
+            {/each}
+        {/if}
+    </div>
 </div>
