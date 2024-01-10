f0fb833808e45e3b095fa604ee460ceadab79cfc
Solomon Montagno <sjmontagno@gmail.com>
2024-01-10 18:38:19 -0500

    Added side nav bar with correct routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     import { AppBar, AppShell } from '@skeletonlabs/skeleton'
     import type { AfterNavigate } from '@sveltejs/kit'
     import { afterNavigate } from '$app/navigation'
-
+    import { AppRail, AppRailAnchor} from '@skeletonlabs/skeleton'
     // Scroll to Top on Navigation
     afterNavigate((params: AfterNavigate) => {
         const isNewPage = params.from?.url.pathname !== params.to?.url.pathname
@@ -12,16 +12,23 @@
             elemPage.scrollTop = 0
         }
     })
+
+let currentTile: number = 0
 </script>
+
 
 <AppShell>
     <svelte:fragment slot="header">
         <AppBar>Hazy Goods</AppBar>
     </svelte:fragment>
-    <svelte:fragment slot="sidebarLeft">
-        <!-- Hidden below Tailwind's large breakpoint -->
-        <div id="sidebar-left" class="hidden lg:block">Sidebar</div>
+    
+<AppRail>
+    <svelte:fragment slot="lead">
+    <AppRailAnchor href="/">Home</AppRailAnchor>
+    <AppRailAnchor href="/dispensary">Dispensary List</AppRailAnchor>
+    <AppRailAnchor href="/dispensary/add">Add New Dispensary</AppRailAnchor>
     </svelte:fragment>
+</AppRail>
     <main>
         <slot />
     </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,14 @@
     export let data
 </script>
 
-<pre>{JSON.stringify(data.thing1, null, 2)}</pre>
+
+
+<ul class="list list-flex pl-20 self-auto">
+	<li>
+		<span>(icon)</span>
+		<span class="flex-auto"><pre>{JSON.stringify(data.thing1, null, 2)}</pre></span>
+	</li>
+	<!-- ... -->
+</ul>
+
+
