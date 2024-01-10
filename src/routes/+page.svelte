632d9773b1d742d1100104a45adb5bc4c0b85009
Solomon Montagno <sjmontagno@gmail.com>
2024-01-10 18:38:19 -0500

    Added side nav bar with correct routing
    Add Icons
    
    fixed formatting issue
    
    fixed broken import to fix validate function
    
    A few minor display tweaks
    
    removed ml-24 on pages and added it to slot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,12 @@
     export let data
 </script>
 
-<pre>{JSON.stringify(data.thing1, null, 2)}</pre>
+<ul class="mt-8">
+    <li>
+        <span>(icon)</span>
+        <span class="flex-auto"
+            ><pre>{JSON.stringify(data.thing1, null, 2)}</pre></span
+        >
+    </li>
+    <!-- ... -->
+</ul>
