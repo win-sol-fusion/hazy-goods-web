68a791b48990fa6f3ddbf608f175d0523dfe6029
Solomon Montagno <sjmontagno@gmail.com>
2024-01-23 12:37:38 -0500

    Added side nav bar with correct routing (#15)
    Add Icons
    
    fixed formatting issue
    
    fixed broken import to fix validate function
    
    A few minor display tweaks
    
    removed ml-24 on pages and added it to slot

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
     import { AppBar, AppShell } from '@skeletonlabs/skeleton'
     import type { AfterNavigate } from '@sveltejs/kit'
     import { afterNavigate } from '$app/navigation'
+    import { AppRail, AppRailAnchor } from '@skeletonlabs/skeleton'
+    import { Icon } from '@steeze-ui/svelte-icon'
+    import { BuildingStorefront } from '@steeze-ui/heroicons'
+    import { PlusCircle } from '@steeze-ui/heroicons'
+    import { ClipboardDocumentList } from '@steeze-ui/heroicons'
 
     // Scroll to Top on Navigation
     afterNavigate((params: AfterNavigate) => {
@@ -12,17 +17,48 @@
             elemPage.scrollTop = 0
         }
     })
+
+    let currentTile: number = 0
 </script>
 
 <AppShell>
     <svelte:fragment slot="header">
         <AppBar>Hazy Goods</AppBar>
     </svelte:fragment>
+
     <svelte:fragment slot="sidebarLeft">
-        <!-- Hidden below Tailwind's large breakpoint -->
-        <div id="sidebar-left" class="hidden lg:block">Sidebar</div>
+        <AppRail class="fixed">
+            <svelte:fragment slot="lead">
+                <AppRailAnchor href="/">
+                    <div class="ml-7">
+                        <Icon
+                            src={BuildingStorefront}
+                            size="20px"
+                            theme="outline"
+                        />
+                    </div>
+                    Home
+                </AppRailAnchor>
+                <AppRailAnchor href="/dispensary">
+                    <div class="ml-7">
+                        <Icon
+                            src={ClipboardDocumentList}
+                            size="20px"
+                            theme="outline"
+                        />
+                    </div>
+                    Dispensary List
+                </AppRailAnchor>
+                <AppRailAnchor href="/dispensary/add">
+                    <div class="ml-7">
+                        <Icon src={PlusCircle} size="20px" theme="outline" />
+                    </div>
+                    Add New Dispensary
+                </AppRailAnchor>
+            </svelte:fragment>
+        </AppRail>
     </svelte:fragment>
-    <main>
+    <div class="ml-24">
         <slot />
-    </main>
+    </div>
 </AppShell>
